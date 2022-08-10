eb9a9a17511cc71b53396d7ddd70fe9321fe5d44
Riku Yamamoto <ymmtr666@gmail.com>
2022-08-11 03:31:28 +0900

    スマホフレームを画像からCSSに変更

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Block from './components/Block';
-import smartphone from './img/smartphone.png';
 
 function App() {
 
   return (
     <div className="App">
       <div className='frame'>
-        <img className='frame-img' src={smartphone}></img>
+        <div className='frame-img'></div>
+
       </div>
       <header className="App-header main">
         <Block />
