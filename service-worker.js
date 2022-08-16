3d49a40e8324522a0ab0e71eb825871c23c1b2ad
Riku Yamamoto <ymmtr666@gmail.com>
2022-08-17 01:11:59 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b92d66511dc0b4775b6b94ba114a013b.js"
+  "/saizeriya-gacha/precache-manifest.17a90a59b1eafe49ae6a9c7de2423bcc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/saizeriya-gacha/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
