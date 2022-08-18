25522a91b8fbe386e86b7b9349c21963ec8f32aa
Riku Yamamoto <ymmtr666@gmail.com>
2022-08-19 04:18:24 +0900

    fix unittest

--- src/utils/gacha.spec.ts
+++ src/utils/gacha.spec.ts
@@ -20,13 +20,13 @@ const randomizer = new Random(8282);
 
 test("予算に応じた商品が選ばれる", () => {
   const expected = [{ name: "name", orderId: "2", price: 500 }];
-  const actual = simple.choise(items, 500, randomizer);
+  const actual = simple.choise(items, 500, randomizer, {});
 
   expect(actual).toEqual(expected);
 });
 
 test("予算に合う商品がない", () => {
-  const actual = simple.choise(items, 450, randomizer);
+  const actual = simple.choise(items, 450, randomizer, {});
 
   expect(actual).toEqual([]);
 });
@@ -34,10 +34,9 @@ test("予算に合う商品がない", () => {
 test("複数選択される確認", () => {
   const expected = [
     { name: "name", orderId: "2", price: 500 },
-    { name: "name", orderId: "2", price: 500 },
-    { name: "name", orderId: "2", price: 500 },
+    { name: "name", orderId: "1", price: 1500 },
   ];
-  const actual = simple.choise(items, 1500, randomizer);
+  const actual = simple.choise(items, 2000, randomizer, {});
 
   expect(actual).toEqual(expected);
 });
